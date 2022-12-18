9fca4cf202536ae86104420bc1c3e36c171c6ae7
Thiago Nascimento <nascimentoliveira@gmail.com>
2022-12-18 15:46:33 -0300

    feat: get user links

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -7,19 +7,42 @@ import {
 export async function getUserShortenedUrls(req, res) {
 
   try {
-    const userShortenedUrls = (await connection.query(`
+    const [userShortenedUrls] = (await connection.query(`
     SELECT 
-      users.id, users.name,  
-      json_build_object(
-        url.id, url."shortUrl", url, "visitCount"
-      ) AS "shortenedUrls"
+      users.id, users.name, SUM(urls."visitCount") AS "visitCount", (
+        SELECT 
+          json_agg(json_build_object(
+            'id', url.id, 
+            'shortUrl', url."shortUrl", 
+            'url', url.url, 
+            'visitCount', url."visitCount"
+          )) AS "shortenedUrls"
+        FROM
+          urls AS url
+      ) 
     FROM  
       urls
     JOIN  
       users 
     ON 
-      users.id=urls."userId";`
-    ));
+      users.id=urls."userId"
+    GROUP BY
+      users.id;`
+    )).rows;
+
+    /* const [userShortenedUrls] = (await connection.query(`
+        SELECT 
+        json_agg(json_build_object(
+            'id', urls.id, 
+            'shortUrl', urls."shortUrl", 
+            'url', urls.url, 
+            'visitCount', urls."visitCount"
+          )) AS "shortenedUrls"
+        FROM
+          urls
+       
+    ;`
+    )).rows; */
 
     res.status(200).send(userShortenedUrls);
 
--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,7 +1,11 @@
 import { Router } from 'express';
+import { getUserShortenedUrls } from '../controllers/users.controller.js';
+import { tokenValid } from '../middlewares/tokenValid.middleware.js';
 
 export const users = Router();
 
 users.get(
-  '/users/me'
+  '/users/me',
+  tokenValid,
+  getUserShortenedUrls
 );
