e8245cd9b858d6856f9cc5c5a1e75e46789e762e
Thiago Oliveira <nascimentoliveira@gmail.com>
2023-05-20 18:18:43 -0300

    refact: remove health router

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 
 import loadEnv from "./envs.js";
-import healthRouter from "./routes/healthRouter.js";
 import routers from "./routes/index.js";
 
 loadEnv();
@@ -10,7 +9,6 @@ loadEnv();
 export const app = express();
 app.use(cors())
   .use(express.json())
-  .get("*", healthRouter)
   .use("/api", routers);
 
 export default app;
