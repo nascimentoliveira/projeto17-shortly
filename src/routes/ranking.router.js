ab3527ab2f3fd321305cd41d50c51d1401f7fa26
Thiago Oliveira <nascimentoliveira@gmail.com>
2023-05-20 17:22:32 -0300

    refact: ranking router

--- src/routes/ranking.router.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getRanking } from '../controllers/ranking.controller.js';
-
-export const ranking = Router();
-
-ranking.get(
-  '/ranking',
-  getRanking
-);
---
+++ src/routes/rankingRouter.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+import { getRanking } from "../controllers/ranking.controller.js";
+
+export const ranking = Router();
+
+ranking.get("/ranking", getRanking);
+
+export default ranking;
+//
