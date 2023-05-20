264589a258b5b8f91d6a2d45180c46b4a4f479bb
Thiago Oliveira <nascimentoliveira@gmail.com>
2023-05-20 18:15:33 -0300

    refact: index routers

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,21 @@
 import { Router } from "express";
 
+import healthRouter from "./healthRouter.js";
 import users from "./usersRouter.js";
 import auth from "./authRouter.js";
 import urls from "./urlsRouter.js";
 import ranking from "./rankingRouter.js";
+import { redirectToLink } from "../controllers/urls.controller.js";
+import { shortUrlValid } from "../middlewares/url.middleware.js";
 
 export const routers = Router();
 routers
-  .use(users)
-  .use(auth)
-  .use(urls)
-  .use(ranking);
+  .use("/health", healthRouter)
+  .use("/users", users)
+  .use("/auth", auth)
+  .use("/urls", urls)
+  .use("/ranking", ranking)
+  .get("/:shortUrl", shortUrlValid, redirectToLink);
 
 export default routers;
 //
