18ba23384f6b11b27ef1b8ead3aae3a23f586e9c
Thiago Oliveira <nascimentoliveira@gmail.com>
2023-05-20 18:16:48 -0300

    feat: create check database connection function

--- src/database/database.js
+++ src/database/database.js
@@ -1,10 +1,24 @@
-import pkg from 'pg';
-import dotenv from 'dotenv';
+import pkg from "pg";
+import dotenv from "dotenv";
 
 dotenv.config();
+
 const { Pool } = pkg;
 
 export const connection = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: true
 });
+
+export function checkDatabaseConnection() {
+  return new Promise((resolve, reject) => {
+    connection.connect((error) => {
+     if (error) {
+       resolve("disconnected");
+     } else {
+       resolve("connected");
+     }
+   });
+ });
+}
+//
