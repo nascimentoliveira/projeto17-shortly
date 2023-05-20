ef33ffaa7b05fe687684842315f3358523554046
Thiago Oliveira <nascimentoliveira@gmail.com>
2023-05-20 18:19:05 -0300

    refact: server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
-import { app } from './app.js';
+import { app } from "./app.js";
 
 const port = process.env.PORT || 4000;
 
 app.listen(port, function (err) {
   if (err) {
-    console.log('Failed to start the server -', err);
+    /* eslint-disable-next-line no-console */
+    console.log("Failed to start the server -", err);
   }
-  console.log('APP Server listening on port', port);
+  /* eslint-disable-next-line no-console */
+  console.log("Server listening on port", port);
 });
+//
