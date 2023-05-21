11dd62807b52ea7e228624628f9043708f872b08
Thiago Oliveira <nascimentoliveira@gmail.com>
2023-05-21 17:02:16 -0300

    refactor: database schema and connection

--- src/database/createDatabase.sql
+++ src/database/createDatabase.sql
@@ -15,21 +15,21 @@ CREATE TABLE "sessions" (
 	"createdAt" TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE "usersUrls" (
+CREATE TABLE "shortURLs" (
 	"id" SERIAL PRIMARY KEY,
 	"userId" INTEGER NOT NULL,
-	"urlId" INTEGER NOT NULL,
-	"shortUrl" TEXT UNIQUE NOT NULL,
+	"bigURLId" INTEGER NOT NULL,
+	"shortURL" TEXT UNIQUE NOT NULL,
 	"visitCount" INTEGER NOT NULL DEFAULT 0
 );
 
-CREATE TABLE "urls" (
+CREATE TABLE "bigURLs" (
 	"id" SERIAL PRIMARY KEY,
-	"bigUrl" TEXT UNIQUE NOT NULL,
+	"bigURL" TEXT UNIQUE NOT NULL,
 	"createdAt" TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 ALTER TABLE "sessions" ADD CONSTRAINT "sessions_fk0" FOREIGN KEY ("userId") REFERENCES "users"("id");
 
-ALTER TABLE "usersUrls" ADD CONSTRAINT "usersUrls_fk0" FOREIGN KEY ("userId") REFERENCES "users"("id");
-ALTER TABLE "usersUrls" ADD CONSTRAINT "usersUrls_fk1" FOREIGN KEY ("urlId") REFERENCES "urls"("id");
+ALTER TABLE "shortURLs" ADD CONSTRAINT "shortURLs_fk0" FOREIGN KEY ("userId") REFERENCES "users"("id");
+ALTER TABLE "shortURLs" ADD CONSTRAINT "shortURLs_fk1" FOREIGN KEY ("bigURLId") REFERENCES "bigURLs"("id");
--- src/database/database.js
+++ src/database/database.js
@@ -5,20 +5,22 @@ dotenv.config();
 
 const { Pool } = pkg;
 
-export const connection = new Pool({
+export const connectionDB = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: true
 });
 
 export function checkDatabaseConnection() {
   return new Promise((resolve, reject) => {
-    connection.connect((error) => {
-     if (error) {
-       resolve("disconnected");
-     } else {
-       resolve("connected");
-     }
-   });
- });
+    connectionDB.connect((error) => {
+      if (error) {
+        resolve("disconnected");
+      } else {
+        resolve("connected");
+      }
+    });
+  });
 }
+
+export default connectionDB;
 //
