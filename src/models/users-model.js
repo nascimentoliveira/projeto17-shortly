f7fc1ecfcf47bf2ae8f9b21b95543ee3f2fba8c0
Thiago Oliveira <nascimentoliveira@gmail.com>
2023-05-21 16:58:32 -0300

    refactor: users model

--- src/models/users-model.js
+++ src/models/users-model.js
@@ -1,8 +1,10 @@
-import joi from 'joi';
+import joi from "joi";
 
-export const signUpSchema = joi.object({
+const usersSchema = joi.object({
   name: joi.string().required(),
   email: joi.string().email().required(),
   password: joi.string().required(),
-  confirmPassword: joi.string().required()
 });
+
+export default usersSchema;
+//
