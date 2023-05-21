c858026b6de0034392054f26d7d759f1ed40f8d6
Thiago Oliveira <nascimentoliveira@gmail.com>
2023-05-21 16:56:54 -0300

    refactor: ranking controller

--- src/controllers/ranking-controller.js
+++ src/controllers/ranking-controller.js
@@ -1,19 +1,23 @@
 import httpStatus from "http-status";
 
-import { userRepository } from "../repositories/user.repository.js";
+import usersRepository from "../repositories/users-repository.js";
 
 export async function getRanking(req, res) {
   try {
-    const ranking = (await userRepository.getRankingUsers()).rows;
-
+    const ranking = (await usersRepository.getRankingUsers()).rows;
     res.status(httpStatus.OK).send(ranking);
-
   } catch (err) {
     /* eslint-disable-next-line no-console */
-    console.error(MESSAGES.INTERNAL_SERVER_ERROR, err);
-    res.status(httpStatus.INTERNAL_SERVER_ERROR).send({ 
-      error: "Desculpe, ocorreu um erro interno no servidor. Por favor, tente novamente mais tarde.",
+    console.error(err);
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
+      error: "Ocorreu um erro interno no servidor. Tente novamente mais tarde.",
     });
   }
 }
+
+const rankingController = {
+  getRanking,
+};
+
+export default rankingController;
 //
