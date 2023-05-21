a45aa60605cf593e6e59cb6dced32782efdc257b
Thiago Oliveira <nascimentoliveira@gmail.com>
2023-05-21 17:03:37 -0300

    remove contansts and token middleware

--- src/constants.js
+++
@@ -1,9 +0,0 @@
-export const MESSAGES = {
-  INTERNAL_SERVER_ERROR: 'An error has occurred:',
-  CLIENT_SERVER_ERROR: 'Não foi possível processar a sua solicitação.',
-  FORMAT_ERROR: 'Body não tem o formato esperado!'
-};
-
-export const ROUNDS_ENCRYPT = 12;
-
-export const DAYS_TOKEN_EXPIRE = 7;
